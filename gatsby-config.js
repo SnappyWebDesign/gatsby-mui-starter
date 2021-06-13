4b1779f741249bdc8ad833efd37545364f9316e2
SnappyWebDesign <83241615+SnappyWebDesign@users.noreply.github.com>
2021-06-13 13:15:09 -0400

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `Gatsby Material-UI Starter`,
+    description: `Gatsby/Material-UI Template via SnappyWebDesign.net`,
+    author: `@snappy_web`,
+  },
+
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-plugin-material-ui`,
+    `gatsby-plugin-react-helmet`,
+  ],
+}
