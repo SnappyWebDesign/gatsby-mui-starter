a5e20cab50b94825da24717cd4c2c40dbb0532a2
SnappyWebDesign <83241615+SnappyWebDesign@users.noreply.github.com>
2021-06-14 10:24:38 -0400

    + SSR
    + gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+import Layout from "./src/components/UI/Layout"
+
+export function wrapRootElement({ element, props }) {
+  return <Layout {...props}>{element}</Layout>
+}
